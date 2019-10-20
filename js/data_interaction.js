6cff317a9c704a89315f4d538a3e13980ab24ed2
Tom A. Vibeto <tom@onlyhuman.dk>
2019-10-21 00:48:12 +0200

    Add actions for interacting with objects.

---
+++ js/data_interaction.js
@@ -0,0 +1,104 @@
+data_interaction = [
+    {
+        title: "Weapons",
+        icon: "crossed-swords",
+        subtitle: "Draw or sheathe",
+        description: "",
+        reference: "PHB, pg. 190.",
+        bullets: [
+            "Draw one weapon. To draw two weapons will require an action or a feat.",
+            "Sheathe one weapon. Dropping the weapon is free."
+        ]
+    },
+    {
+        title: "Backpack",
+        icon: "hand",
+        subtitle: "Retrieve or stove",
+        description: "",
+        reference: "",
+        bullets: [
+            "Retrieve an item from your backpack.",
+            "Fish a few golds from your pouch."
+        ]
+    },
+    {
+        title: "Retrieve",
+        icon: "magic-swirl",
+        subtitle: "Pick up an item",
+        description: "",
+        reference: "",
+        bullets: [
+            "Pick up an axe on the ground.",
+            "Take a book from a shelf.",
+            "Take a bauble from a table."
+        ]
+    },
+    {
+        title: "Wear",
+        icon: "sprint",
+        subtitle: "Equip or unequip",
+        description: "",
+        reference: "",
+        bullets: [
+            "Remove a ring on your finger.",
+            "Don a mask.",
+            "Pull up the hood of your cloak over your head."
+        ]
+    },
+    {
+        title: "Consume",
+        icon: "journey",
+        subtitle: "Food or drinks",
+        description: "",
+        reference: "",
+        bullets: [
+            "Stuff some food into your mouth.",
+            "Drink all the ale in a flagoon."
+        ]
+    },
+    {
+        title: "Place",
+        icon: "aura",
+        subtitle: "Put an item somewhere.",
+        description: "Dropping stuff is free.",
+        reference: "",
+        bullets: [
+            "Plant a banner in the ground."
+        ]
+    },
+    {
+        title: "Interact",
+        icon: "manacles",
+        subtitle: "Open door",
+        description: "",
+        reference: "",
+        bullets: [
+            "Open a unlocked door",
+            "Throw a lever or a switch.",
+            "Extinguish a small flame.",
+            "Turn a key in a lock.",
+            "Tap the floor with a 10-foot pole."
+        ]
+    },
+    {
+        title: "Sense",
+        icon: "manacles",
+        subtitle: "Smell, listen, hear",
+        description: "",
+        reference: "",
+        bullets: [
+            "Put your ear to the door.",
+            "Sniff at a meal."
+        ]
+    },
+    {
+        title: "Pass",
+        icon: "telepathy",
+        subtitle: "Hand to another character",
+        description: "",
+        reference: "",
+        bullets: [
+            "Hand a bottle to your ally."
+        ]
+    }
+]
